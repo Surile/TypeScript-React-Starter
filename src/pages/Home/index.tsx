b522aed25a29e35392664db53e84d059ada0762d
Suriel <mhecy98@gmail.com>
2020-05-22 20:11:43 +0800

    react hooks

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,16 +1,29 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import Layout from '@/components/Layout'
+import './index.scss'
 
 export default () => {
     const [count, setCount] = useState(0)
+
+    const add = () => setCount(count + 1)
+
+    const minus = () => {
+        if (count == 1) return
+        setCount(count - 1)
+    }
     return (
         <Layout>
             <div
                 style={{
                     padding: '10px',
                 }}>
-                <button onClick={() => setCount(count + 1)}>+</button>
-                home world {count}
+                <div>
+                    <p>You clicked {count} times</p>
+                    <div className="btn-flex">
+                        <button onClick={add}>加</button>
+                        <button onClick={minus}>减</button>
+                    </div>
+                </div>
             </div>
         </Layout>
     )
